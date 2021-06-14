e527304d563062005b6fa01556e569aa3bda9853
仓鼠 <zhuxx2003@gmail.com>
2021-06-14 16:58:46 +0800

    Fix empty

--- reconvert_paratranz.js
+++ reconvert_paratranz.js
@@ -11,14 +11,14 @@ try {
         item.speaker = item.context.match(/说话人: (\S+)/)[1] + ' '
         if (item.speaker === '无 ')
           item.speaker = null
-        result += `# ${item.origin}\ntranslate chinese ${item.key}:\n\n    # ${item.speaker ?? ''}"${item.original}"\n    ${item.speaker ?? ''}"${item.translation ?? ''}"\n\n`
+        result += `# ${item.origin}\ntranslate chinese ${item.key}:\n\n    # ${item.speaker ?? ''}"${item.original}"\n    ${item.speaker ?? ''}"${item.translation !== '' ? item.translation : item.original}"\n\n`
       }
     }
     if (fs.existsSync(`paratranz/${name}_strings.json`)) {
       let strings = JSON.parse(fs.readFileSync(`paratranz/${name}_strings.json`, 'utf-8'))
       result += 'translate chinese strings:\n\n'
       for (let item of strings)
-        result += `    # ${item.key}\n    old "${item.original}"\n    new "${item.translation ?? ''}"\n\n`
+        result += `    # ${item.key}\n    old "${item.original}"\n    new "${item.translation !== '' ? item.translation : item.original}"\n\n`
     }
     fs.writeFileSync(`game/tl/chinese/${file}`, result)
   }
