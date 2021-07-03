639e2072945bf1faba4ae9f5ac7edf16a5831c46
H4M5TER <zhuxx2003@gmail.com>
2021-07-03 19:14:00 +0800

    Add reconverter

---
+++ reconvert_paratranz.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+try {
+  for (let file of fs.readdirSync('game/tl/chinese').filter(file => /\.rpy$/.test(file))) {
+    let name = file.match(/^(.+)\.rpy$/)[1]
+    let result = `# Translation updated at ${new Date()}\n\n`
+    if (fs.existsSync(`paratranz/${name}.json`)) {
+      let dialogs = JSON.parse(fs.readFileSync(`paratranz/${name}.json`, 'utf-8'))
+      for (let item of dialogs) {
+        item.origin = item.context.match(/game\/.+$/)
+        item.speaker = item.context.match(/说话人: (\S+)/)[1] + ' '
+        if (item.speaker === '无 ')
+          item.speaker = null
+        result += `# ${item.origin}\ntranslate chinese ${item.key}:\n\n    # ${item.speaker ?? ''}"${item.original}"\n    ${item.speaker ?? ''}"${item.translation ?? ''}"\n\n`
+      }
+    }
+    if (fs.existsSync(`paratranz/${name}_strings.json`)) {
+      let strings = JSON.parse(fs.readFileSync(`paratranz/${name}_strings.json`, 'utf-8'))
+      result += 'translate chinese strings:\n\n'
+      for (let item of strings)
+        result += `    # ${item.key}\n    old "${item.original}"\n    new "${item.translation ?? ''}"\n\n`
+    }
+    fs.writeFileSync(`game/tl/chinese/${file}`, result)
+  }
+} catch (error) {
+  console.error(error)
+}
